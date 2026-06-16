3f1390ef0276d327cae416e1e8f46355f2436b62
joeltheai <55612521+joeltheai@users.noreply.github.com>
2026-06-16 20:31:37 +0530

    added read_keyword implementation

--- src/main.rs
+++ src/main.rs
@@ -53,12 +53,92 @@ impl Lexer {
             None
         }
     }
+
+    fn read_keyword(&mut self, keyword: &str) {
+        for expected in keyword.as_bytes() {
+            match self.current() {
+                Some(c) if c == *expected => self.advance(),
+                Some(c) => panic!(
+                    "Unexpected character '{}' while reading keyword '{}'",
+                    c as char, keyword
+                ),
+                None => panic!(
+                    "Unexpected end of input while reading keyword '{}'",
+                    keyword
+                ),
+            }
+        }
+    }
 }
 
 fn tokenize(input: &str) -> Vec<Token> {
-    todo!()
-}
+    let mut lexer = Lexer::new(input);
+    let mut tokens = Vec::new();
 
+    loop {
+        match lexer.current() {
+            None => break,
+
+            Some(b' ') | Some(b'\t') | Some(b'\n') | Some(b'\r') => {
+                lexer.advance();
+            }
+
+            Some(b'{') => {
+                tokens.push(Token::LeftBrace);
+                lexer.advance();
+            }
+            Some(b'}') => {
+                tokens.push(Token::RightBrace);
+                lexer.advance();
+            }
+            Some(b'[') => {
+                tokens.push(Token::LeftBracket);
+                lexer.advance();
+            }
+            Some(b']') => {
+                tokens.push(Token::RightBracket);
+                lexer.advance();
+            }
+            Some(b':') => {
+                tokens.push(Token::Colon);
+                lexer.advance();
+            }
+            Some(b',') => {
+                tokens.push(Token::Comma);
+                lexer.advance();
+            }
+
+            Some(b'"') => {
+                let s = lexer.read_string();
+                tokens.push(Token::StringToken(s));
+            }
+
+            Some(b't') => {
+                lexer.read_keyword("true");
+                tokens.push(Token::True);
+            }
+            Some(b'f') => {
+                lexer.read_keyword("false");
+                tokens.push(Token::False);
+            }
+            Some(b'n') => {
+                lexer.read_keyword("null");
+                tokens.push(Token::Null);
+            }
+
+            Some(b'-') | Some(b'0'..=b'9') => {
+                let n = lexer.read_number();
+                tokens.push(Token::NumberToken(n));
+            }
+
+            Some(c) => {
+                panic!("Unexpected character: {}", c as char);
+            }
+        }
+    }
+
+    tokens
+}
 fn parse(tokens: &[Token]) -> JsonValue {
     todo!()
 }
